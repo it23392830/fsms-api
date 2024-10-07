661177a1a289da9e3530e028f9432476b4ad122f
it23392830 <it23392830@my.sliit.lk>
2024-10-07 20:39:42 +0530

    Dev

--- fsms-api/src/main/java/com/fsms/api/Application.java
+++ fsms-api/src/main/java/com/fsms/api/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FsmsApiApplication {
+	//test
 
 	public static void main(String[] args) {
 		SpringApplication.run(FsmsApiApplication.class, args);
